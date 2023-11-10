55c08d69bf04c8375073870925ba69bba8a48a04
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-11-10 19:03:43 +0000

    added the number of months in a console log

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Dataset
 var finances = [
     ['Jan-2010', 867884],
     ['Feb-2010', 984655],
@@ -86,5 +87,6 @@ var finances = [
     ['Jan-2017', 138230],
     ['Feb-2017', 671099],
   ];
-  
-  
+
+//Number Of Months is the array's length
+console.log(finances.length);
